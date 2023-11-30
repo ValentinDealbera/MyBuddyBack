71a285eacb1b94c6bd1d21333b8ac8a6c4045eaf
Valentin Dealbera <111397786+ValentinDealbera@users.noreply.github.com>
2023-11-30 10:11:08 -0300

    Update index.js

--- api/src/routes/index.js
+++ api/src/routes/index.js
@@ -10,7 +10,12 @@ const deleteDog = require('../controllers/deleteDog');
 const router = Router();
 
 // Configurar los routers
-router.get('/dogs', getAllDogs)
+try{
+  router.get('/dogs', getAllDogs)
+}
+catch(error){
+  console.log(error)
+}
 
 router.get('/dogs/name', getDogsByName)
 
